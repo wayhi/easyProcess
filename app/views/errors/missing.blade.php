ca381e90b7a36e64681341439e5b31d5fc871ca1
wayhi <JW@shiwenbodeMacBook-Pro.local>
2015-05-08 00:38:16 +0800

    0507

---
+++ app/views/errors/missing.blade.php
@@ -0,0 +1,8 @@
+@extends('main')
+@section('main')
+
+<div>
+<h2 align='center'>Page Not Found!<br>所访问页面不存在！</h2>
+</div>
+
+@stop
---
+++ app/views/main.blade.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>easyProcess</title>
+
+  @include('admin._partials.assets')
+
+</head>
+<body>
+
+  
+  <div class="container">
+  @yield('main')
+  	
+
+ 	  <hr>
+	     <div class="footer">
+        <p>&copy; easyProcess.cn 2014</p>
+      </div>
+  </div>
+
+</body>
+</html>
